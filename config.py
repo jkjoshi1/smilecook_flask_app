f4dc8508937bf354bda8a12b6b2040826fabf9f3
massoom <massoom@massoom.com>
2020-11-30 19:36:47 +0530

    added database models and connected with PostgreSQL

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config:
+
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://python_api:joshi@localhost/smilecook_tryagain'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- models/recipe.py
+++ models/recipe.py
@@ -1,3 +1,4 @@
+from extensions import db
 recipe_list = []
 
 
@@ -10,7 +11,22 @@ def get_last_id():
     return last_recipe.id+1
 
 
-class Recipe:
+class Recipe(db.Model):
+
+    __tablename__ = 'recipe'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    description = db.Column(db.String(200))
+    no_of_serving = db.Column(db.Integer, nullable=False)
+    cook_time = db.Column(db.Integer)
+    direction = db.Column(db.String(1000))
+    is_publish = db.Column(db.Boolean, default=False)
+    updated_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    created_at = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+
+    user_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
+
     def __init__(self, name, description, no_of_serving, cook_time, direction):
         self.id = get_last_id()
         self.name = name
